7377ba6068304ff9d9100f3f04daa867ca506c5b
Selma Brattli <selma@brattli.nu>
2024-08-27 10:11:22 +0200

    sanetzing the input field

--- src/comps/chat/Conversation.jsx
+++ src/comps/chat/Conversation.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect, useRef } from 'react';
 import styles from '../../styles/chat/Conversation.module.css'
 import { DecodeJwtContext } from '../../context/DecodeJwtContextProvider';
+import DOMPurify from "dompurify";
 
 const Conversation = () => {
 
@@ -8,9 +9,12 @@ const Conversation = () => {
     const jwt = sessionStorage.getItem("token");
     const decodedJwt = JSON.parse(atob(jwt.split('.')[1]));
 
-    // Fetching all messages
-
     const [messages, setMessages] = useState([])
+    
+    const inputValue = useRef()
+    const [sentMsg, setSentMsg] = useState(false); 
+
+    // Fetching all messages
     
     useEffect(() => {
         fetch(import.meta.env.VITE_API_URL + '/messages', {
@@ -32,7 +36,7 @@ const Conversation = () => {
         .catch(error => {
             console.error('There was a problem with your fetch operation:', error);
         });
-    }, []);
+    }, [sentMsg]);
 
     // if (!decodedJwt) {
     //     return <p>Loading...</p>;
@@ -41,12 +45,9 @@ const Conversation = () => {
 
     // Post message
 
-    // const [newMessage, setNewMessage] = useState('')
-    const inputValue = useRef()
-
     const newMessageHandler = () => {
         if (inputValue && inputValue.current.value.length > 0){
-            const newMessage = inputValue.current.value;
+            const newMessage = DOMPurify.sanitize(inputValue.current.value);
             console.log(newMessage)
 
             fetch(import.meta.env.VITE_API_URL + '/messages', {
@@ -66,9 +67,9 @@ const Conversation = () => {
                 }
                 return response.json();
             })
-            // IF YOU WANT A USER RESPONSIVE MESSAGE
-            // .then(data => {
-            // })
+            .then(data => {
+                setSentMsg(prev => !prev);
+            })
             .catch(error => {
                 console.error('There was a problem with your fetch operation:', error);
             });
@@ -100,12 +101,9 @@ const Conversation = () => {
             <div>
                 <input 
                     type="text" 
-                    // onChange={(e) => setNewMessage(e.target.value)}
                     ref={inputValue}
                 />
                 <button onClick={newMessageHandler}>Send</button>
-
-                {/* Test: {newMessage} */}
             </div>
         </div>
     )
